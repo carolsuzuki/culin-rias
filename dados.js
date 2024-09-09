80dd974d95b0d0d36482c61489a418ea9bff0340
carolsuzuki <carolpsuzuki@gmail.com>
2024-09-09 18:41:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Italiano",
+        descricao : "Imagine saborear uma massa al dente, envolvida em um molho suculento e perfumado, finalizada com queijo parmesao fresco. Ou sentir a explosao de sabores de uma pizza napolitana, com sua massa leve e crocante, coberta por tomates frescos, mussarela derretida e um toque de manjericao.",
+        link : "https://pt.wikipedia.org/wiki/Culinária_da_Itália",
+        tags: "macarrão macarrao lasanha nhoque"
+    },
+    {
+        titulo: "Japonês",
+        descricao: "A culinária japonesa é famosa por sua frescura, equilíbrio de sabores e apresentação visual impecável. Experimente um delicioso sushi, preparado com arroz japonês, peixe cru e algas marinhas, ou um ramen quente e reconfortante, com macarrão em um caldo saboroso e acompanhamentos variados.",
+        link: "https://pt.wikipedia.org/wiki/Cozinha_japonesa",
+        tags: "sashimi tempurá guiosa"
+    },
+    {
+        titulo: "Mexicano",
+        descricao: "A culinária mexicana é conhecida por seus sabores intensos e vibrantes, com uma combinação única de ingredientes frescos e especiarias. Saboreie um autêntico taco, com tortilla de milho, carne temperada, guacamole e outros acompanhamentos, ou um delicioso mole, um molho espesso e saboroso feito com chocolate, especiarias e pimentas.",
+        link: "https://pt.wikipedia.org/wiki/Cozinha_mexicana",
+        tags: "guacamole feijão abacate"
+    }
+];
+
